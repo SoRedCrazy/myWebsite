38f08200a4efdb4345379e5228d394a055599420
Soredcrazy <julien.boisgard37@gmail.com>
2025-01-29 19:50:32 +0100

    add refuse add acceptes quotes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const pdfParse = require('pdf-parse');
 const app = express();
 const port = 5000;
+require('dotenv').config({path: __dirname + '/.env'})
 
 app.use(express.static('public'));
 
@@ -86,6 +87,133 @@ app.post('/upload', upload.single('pdf'), async (req, res) => {
   } catch (error) {
     console.error('Error processing PDF:', error);
     res.status(500).send('Error processing PDF');
+  }
+});
+
+app.get('/devis/accepted/:ref', (req, res) => {
+  try {
+    const ref = req.params.ref;
+    let id = null;
+    const url_get = new URL(`https://www.facturation.pro/firms/${process.env.FACTURATION_PROVIDER_FIRMID}/quotes.json`);
+    url_get.searchParams.append('quote_ref', ref );
+
+    fetch(url_get.toString(), {
+      method: 'GET',
+      headers: {
+        'User-Agent': 'boisgard plomberie (boisgardplomberie@gmail.com)',
+        'Authorization': `Basic ` + Buffer.from(process.env.FACTURATION_PROVIDER_ID + ":" + process.env.FACTURATION_PROVIDER_KEY).toString('base64'),
+        'Content-Type': 'application/json'
+      },
+    }).then(async response => {
+      if (!response.ok) {
+        const errorText = await response.text();
+        res.render('error', { message: error.message });
+        return null;
+      }
+      const data = await response.json();
+      if (data.length > 0) {
+        if (data[0].quote_status === 1 || data[0].quote_status === 9) {
+          res.render('error', { message: "Devis déjà accepté ou refusé" });
+          return null;
+        }
+        id = data[0].id;
+        // Nest the second fetch here
+        const url = new URL(`https://www.facturation.pro/firms/${process.env.FACTURATION_PROVIDER_FIRMID}/quotes/${id}.json`);
+        return fetch(url.toString(), {
+          method: 'PATCH',
+          headers: {
+            'User-Agent': 'boisgard plomberie (boisgardplomberie@gmail.com)',
+            'Authorization': `Basic ` + Buffer.from(process.env.FACTURATION_PROVIDER_ID + ":" + process.env.FACTURATION_PROVIDER_KEY).toString('base64'),
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            id: id,
+            quote_status: "1"
+          })
+        });
+      } else {
+        res.render('error', { message: "Devis introuvable" });
+        return null;
+      }
+    })
+    .then(async response => {
+      if (!response) return;
+      if (!response.ok) {
+        const errorText = await response.text();
+        res.render('error', { message: error.message });
+        return null;
+      }
+      res.render('devis-status', { title: "Devis Accepté" ,data: "Devis " + id + " est accepté merci à vous de votre confiance !" });
+    })
+    .catch(error => {
+      res.render('error', { message: error.message });
+    });
+  } catch (error) {
+    res.status(500).send('Error');
+  }
+});
+
+
+app.get('/devis/refused/:ref', upload.single('pdf'), async (req, res) => {
+  try {
+    const ref = req.params.ref;
+    let id = null;
+    const url_get = new URL(`https://www.facturation.pro/firms/${process.env.FACTURATION_PROVIDER_FIRMID}/quotes.json`);
+    url_get.searchParams.append('quote_ref', ref );
+
+    fetch(url_get.toString(), {
+      method: 'GET',
+      headers: {
+        'User-Agent': 'boisgard plomberie (boisgardplomberie@gmail.com)',
+        'Authorization': `Basic ` + Buffer.from(process.env.FACTURATION_PROVIDER_ID + ":" + process.env.FACTURATION_PROVIDER_KEY).toString('base64'),
+        'Content-Type': 'application/json'
+      },
+    }).then(async response => {
+      if (!response.ok) {
+        const errorText = await response.text();
+        res.render('error', { message: error.message });
+        return null;
+      }
+      const data = await response.json();
+      if (data.length > 0) {
+        if (data[0].quote_status === 1 || data[0].quote_status === 9) {
+          res.render('error', { message: "Devis déjà accepté ou refusé" });
+          return null;
+        }
+        id = data[0].id;
+        // Nest the second fetch here
+        const url = new URL(`https://www.facturation.pro/firms/${process.env.FACTURATION_PROVIDER_FIRMID}/quotes/${id}.json`);
+        return fetch(url.toString(), {
+          method: 'PATCH',
+          headers: {
+            'User-Agent': 'boisgard plomberie (boisgardplomberie@gmail.com)',
+            'Authorization': `Basic ` + Buffer.from(process.env.FACTURATION_PROVIDER_ID + ":" + process.env.FACTURATION_PROVIDER_KEY).toString('base64'),
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            id: id,
+            quote_status: "9"
+          })
+        });
+      } else {
+        res.render('error', { message: "Devis introuvable" });
+        return null;
+      }
+    })
+    .then(async response => {
+      if (!response) return;
+      if (!response.ok) {
+        const errorText = await response.text();
+        res.render('error', { message: error.message });
+        return null;
+      }
+      res.render('devis-status', {title: "Devis refusé",  data: "Devis " + id + " est refusé Bonne journée à vous!" });
+    })
+    .catch(error => {
+      res.render('error', { message: error.message });
+    });
+  } catch (error) {
+    res.status(500).send('Error');
   }
 });
 
